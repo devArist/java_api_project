6bcd2727ff31729f5dd1dae6dda0365cf791763f
devArist <50432616+devArist@users.noreply.github.com>
2022-02-22 14:28:33 +0000

    commit

--- activityapi/src/main/java/com/api/project/activityapi/Activity.java
+++ activityapi/src/main/java/com/api/project/activityapi/Activity.java
@@ -4,8 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "activite")
 public class Activity {
     private Integer id;
     private String nom;
